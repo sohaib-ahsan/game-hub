bdff1d839fc8ca952d1ced37fe93fecbdcd1f3a3
sohaib-ahsan <111292668+sohaib-ahsan@users.noreply.github.com>
2023-10-10 23:02:30 +0500

    Handle Games with no images

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,4 +1,7 @@
+import Noimg from "../assets/NoImg.jpg";
+
 const getCroppedImageUrl = (url: string) => {
+  if (!url) return Noimg;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
