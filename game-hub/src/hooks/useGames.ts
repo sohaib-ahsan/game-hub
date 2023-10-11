2a09d8ccfe856ce53d4fe6b05c9adee1b68d27bc
sohaib-ahsan <111292668+sohaib-ahsan@users.noreply.github.com>
2023-10-11 14:22:41 +0500

    Customzie the theme to get darker grays

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -12,6 +12,7 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
@@ -21,7 +22,7 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        odering: gameQuery.sortOrder,
+        ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
       },
     },
